b8f5c4da13383fc0f5a78642a8676cf077cc355d
Haoyu Zhang <haoyuzhang@Haoyus-MacBook-Air.local>
2023-01-11 19:14:06 -0700

    Modified python file Q8

--- lab1.py
+++ lab1.py
@@ -1,5 +1,7 @@
 import requests
 
 print(requests.__version__)
-response = requests.get('http://google.com/')
+response = requests.get('http://www.google.com/')
+response_github = requests.get('https://github.com/HyZ7846/CMPUT404lab1')
 #print(response.text)
+print(response_github.text)
